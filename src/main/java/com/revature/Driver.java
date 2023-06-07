45628dadd7436fbcde056dfc5ee5733c2d0fca89
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-06-07 09:22:31 -0700

    launchers

---
+++ src/main/java/com/revature/Driver.java
@@ -0,0 +1,20 @@
+package com.revature;
+
+import com.revature.utils.JavalinAppConfig;
+import io.javalin.Javalin;
+
+public class Driver {
+
+    // This driver has the sole responsibility of starting our Javalin application, in a perfect world this will likely
+    // contain the ONLY main method in our application
+
+    public static void main(String[] args) {
+
+        // Create a new instance of the Javalin Config class
+        JavalinAppConfig app = new JavalinAppConfig();
+
+        // Start the app with app.start
+        app.start(7070);
+
+    }
+}
--- src/main/java/com/revature/deprecated/Launcher.java
+++ src/main/java/com/revature/deprecated/Launcher.java
@@ -2,24 +2,48 @@ package com.revature.deprecated;
 
 import com.revature.daos.TaskDAO;
 import com.revature.daos.UserDAO;
+import com.revature.models.Task;
+import com.revature.models.User;
 import com.revature.utils.ConnectionUtil;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Launcher {
 
     public static void main(String[] args) {
 
+        /*This is a try-with-resources block
+         A resource (in this case, a DB connection) is opened within the parenthesis
+         Another benefit is that the resource will close for us when the block completes
+         This is helpful for code cleanup and preventing memory leaks*/
         try(Connection conn = ConnectionUtil.getConnection()){
             System.out.println("CONNECTION SUCCESSFUL :)");
         }
-        catch(SQLException e) {
-            System.out.println("Connection Failed :(");
+        catch(SQLException e){
+            System.out.println("Connection Failed :(" );
         }
-        UserDAO uDAO = new UserDAO();
-        TaskDAO tDAO = new TaskDAO();
 
+
+        //calling DAO methods below---------------
+
+        //instantiate a RoleDAO object so we can use its methods
+        UserDAO uDAO = new UserDAO();
+        User tristan = new User("Tristan", "White");
+
+        uDAO.insertUser(tristan);
+
+        //getting a User object by id
+        System.out.println(uDAO.getUserById(1));
+
+        ArrayList<User> userList = uDAO.getAllUsers();
+
+        uDAO.updateUserFirstName("John", 1);
+
+        for (User u : userList) {
+            System.out.println(u);
+        }
 
     }
 
